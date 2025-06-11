fb5c150458d53b47e0135bc8a42f84e6896441fc
VanMinh153 <minhnguyenvh.703@gmail.com>
2025-06-11 17:25:52 +0700

    up Goroutines, Mutex, Buffered_Channel example

---
+++ main/Basic/Goroutines_2/Goroutines_2.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func hello(wgrp *sync.WaitGroup) {
+	fmt.Println("Hello")
+	wgrp.Done()
+}
+
+func main() {
+	//     var wg sync.WaitGroup
+	//     wg.Add(2)
+	//     go hello(&wg)
+	//     go hello(&wg)
+	//     wg.Wait()
+	//     fmt.Println("main function")
+
+	var wg sync.WaitGroup
+	players := []string{"James Harden", "Kyrie Irving", "Kevin Durant"}
+	wg.Add(len(players))
+	for _, player := range players {
+		go func(baller string) {
+			fmt.Printf("printing player %s\n", baller)
+			wg.Done()
+		}(player)
+	}
+	wg.Wait()
+
+}
